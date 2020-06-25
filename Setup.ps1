4d78414bf8a339825f5af7ddece7c5e0930042af
Simon Brathwaite <38538293+sbrathwaite@users.noreply.github.com>
2020-06-25 16:18:33 +0200

    Update Setup.ps1

--- Setup.ps1
+++ Setup.ps1
@@ -1,26 +1,28 @@
 $UninstallFolder = "C:\PandaAvinstallation"
 
-if(!Test-Path $UninstallFolder) {
+if(!(Test-Path $UninstallFolder)) {
     try {
-        New-Item -Path $UninstallFolder -ItemType Directory        
+        New-Item -Path $UninstallFolder -ItemType Directory | Out-Null   
     }
     catch {
         Write-Host "Could not create folder" $Error[0]
     }
+
+    if (![System.IO.File]::Exists("$UninstallFolder\DG_AETHER.exe")) {
+        Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/sbrathwaite/Hors/master/dg_aether/DG_AETHER.exe" -OutFile $UninstallFolder\DG_AETHER.exe    
+    }
+    
+    if (![System.IO.File]::Exists("$UninstallFolder\DG_PANDAPROT8_XX.exe")) {
+        Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/sbrathwaite/Hors/master/dg_aether/DG_PANDAPROT8_XX.exe" -OutFile $UninstallFolder\DG_PANDAPROT8_XX.exe    
+    }
+    
+    
+    # Download Script
+    if (![System.IO.File]::Exists("C:\PandaAvinstallation\uninstaller.exe")) {
+        Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/sbrathwaite/Hors/master/EnableDefender.ps1" -OutFile $UninstallFolder\EnableDefender.ps1
+    }
+    
+    Write-Host "Files downloaded to $UninstallFolder" -ForegroundColor Green
+} else {
+    Write-Warning "Files Already exists in $UninstallFolder";
 }
-
-if (![System.IO.File]::Exists("$UninstallFolder\DG_AETHER.exe")) {
-    Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/sbrathwaite/Hors/master/dg_aether/DG_AETHER.exe" -OutFile $UninstallFolder\DG_AETHER.exe    
-}
-
-if (![System.IO.File]::Exists("$UninstallFolder\DG_PANDAPROT8_XX.exe")) {
-    Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/sbrathwaite/Hors/master/dg_aether/DG_PANDAPROT8_XX.exe" -OutFile $UninstallFolder\DG_AETHER.exe    
-}
-
-
-# Download Script
-if (![System.IO.File]::Exists("C:\PandaAvinstallation\uninstaller.exe")) {
-    Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/sbrathwaite/Hors/master/EnableDefender.ps1" -OutFile $UninstallFolder\EnableDefender.ps1
-}
-
-Write-Host "Files downloaded to $UninstallFolder" -ForegroundColor Green
