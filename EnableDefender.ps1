0c29a249610d1998b541b57d0c7ea1de01fbf9cd
Simon Brathwaite <38538293+sbrathwaite@users.noreply.github.com>
2020-06-25 16:26:33 +0200

    Update EnableDefender.ps1

--- EnableDefender.ps1
+++ EnableDefender.ps1
@@ -1,24 +1,11 @@
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-    Start-Process powershell -Verb RunAs
+    Start-Process powershell -Verb RunAs "iwr -UseBasicParsing https://raw.githubusercontent.com/sbrathwaite/Hors/master/EnableDefender.ps1 | iex"
     exit;
 }
 
 # $pandaProds = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like "Panda *" };
-# if ( -not $pandaProds ) { 
-#     Write-Host "No Panda products were found, skip uninstall";
-# }
-# else {  
-#     foreach ($app in $pandaProds) {    
-#         Write-Host "Uninstalling $($app.Name) in 3 seconds... " -NoNewline;    
-#         Start-Sleep -Seconds 3;    
-#         try {      
-#             $app.Uninstall();      
-#             Write-Host "Done";
-#         }    
-#         catch {      
-#             Write-Host "FAIL" -ForegroundColor Red;    
-#         }  
-#     }
+# if ( $pandaProds ) { 
+#     Write-Host "One or more Panda products are still installed." -ForegroundColor Red;
 # }
 
 # Check if reg keys are set correctly
@@ -27,20 +14,26 @@ $Keys = @(
     "DisableAntiVirus"
 )
 Foreach ($Key in $Keys) {
-    $KeyValue = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Windows Defender" -Name $Key 
+    # Check if keys exists first
+    # Get-MpComputerStatus | Select -Property AntispywareEnabled, AntivirusEnabled
+    $KeyValue = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Windows Defender" -Name $Key
+    if (!Test-Path $KeyValue) {
+        Write-Host "Reg key for AntiSpyware, AntiVirus does not exists, using alternative check...";
+        break;
+    }
     if ($KeyValue -ne 0) {
-        Write-Host "Windows Defender registry key: " $Key " is not configured correctly" -ForegroundColor Red
-        Write-Host "Attempting to configure..."
+        Write-Host "Windows Defender registry key: " $Key " is not configured correctly" -ForegroundColor Red;
+        Write-Host "Attempting to configure...";
         try {
-            Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows Defender" -Name $Key -Value 0 -Type REG_DWORD
+            Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows Defender" -Name $Key -Value 0 -Type REG_DWORD;
         }
         catch {
-            Write-Host "Failed to configure registry key" -ForegroundColor Red
-            Read-Host -Prompt "Press Enter to exit"
+            Write-Host "Failed to configure registry key" -ForegroundColor Red;
+            Read-Host -Prompt "Press Enter to exit";
         }
     }
     else {
-        Write-Host "Windows Defender registry key: " $Key " is configured correctly" -ForegroundColor Green
+        Write-Host "Windows Defender registry key: " $Key " is configured correctly" -ForegroundColor Green;
     }
 }
 
@@ -55,15 +48,15 @@ Foreach ($Key in $Keys) {
 # 6 - Block
 Write-Host "Attempting to configure Defender"
 try {
-    Set-MpPreference -EnableNetworkProtection Enabled
-    Set-MpPreference -HighThreatDefaultAction 2
-    Set-MpPreference -SevereThreatDefaultAction 2
-    Set-MpPreference -DisableRealtimeMonitoring $false
-    Set-MpPreference -SubmitSamplesConsent 3
+    Set-MpPreference -EnableNetworkProtection Enabled | Out-Null
+    Set-MpPreference -HighThreatDefaultAction 2 | Out-Null
+    Set-MpPreference -SevereThreatDefaultAction 2 | Out-Null
+    Set-MpPreference -DisableRealtimeMonitoring $false | Out-Null
+    Set-MpPreference -SubmitSamplesConsent 3 | Out-Null
     Write-Host "Done"
 }
 catch {
-    Write-Host "Failed to configure Windows Defender"
+    Write-Host "Failed to set preferences"
 }
 
 # Enable Windows Firewall on all network profiles
@@ -91,6 +84,26 @@ else {
         Read-Host -Prompt "Press Enter to exit"
         exit
     }
+}
+# Set spotify Firewall exceptions
+# UWP
+# Check if UWP first
+If((Get-NetFirewallRule -Group "Spotify Music") -ne $null) {
+    try {
+        Set-NetFirewallRule -Group "Spotify Music" -Enabled $true -Profile Domain, Public, Private -Direction Outbound -Action Allow    
+    }
+    catch {
+        Write-Host "Failed to let spotify through Firewall"
+    }
+}
+
+# Exe
+
+try {
+    Remove-Item -Path "C:\PandaAvinstallation" -Recurse -Force
+}
+catch {
+    Write-Warning "Could not remove 'PandaAvinstalltion' folder, remove it manually"    
 }
 
 Read-Host -Prompt "Press Enter to restart PC"
