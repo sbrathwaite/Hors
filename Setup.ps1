9ca65d73c5d0c920c4c7e23a6303a3c82f0c79d5
Simon Brathwaite <38538293+sbrathwaite@users.noreply.github.com>
2020-08-12 10:04:31 +0200

    Added Windows Upgrade assistant

--- Setup.ps1
+++ Setup.ps1
@@ -16,6 +16,8 @@ if(!(Test-Path $UninstallFolder)) {
         Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/sbrathwaite/Hors/master/dg_aether/DG_PANDAPROT8_XX.exe" -OutFile $UninstallFolder\DG_PANDAPROT8_XX.exe    
     }
     
+    # Download Upgrade assistant for Windows
+    Invoke-WebRequest -Uri "https://download.microsoft.com/download/8/3/c/83c39dca-2d27-4c24-b98b-0a4d6d921c80/Windows10Upgrade9252.exe" -OutFile "C:\temp\Windows10Upgrade9252.exe";
     
     # Download Script
     if (![System.IO.File]::Exists("C:\PandaAvinstallation\uninstaller.exe")) {
